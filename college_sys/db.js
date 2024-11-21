d5bf886eebcfc10b9a7b131f2c1b20a79b4538cb
Gara Kinkinsoko <kinkingara@outlook.com>
2024-11-21 12:58:19 +0100

    Moved credentials to .env

--- college_sys/db.js
+++ college_sys/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwertyuiop',
-    database: 'college_man_sys'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 connection.connect((err) => {
